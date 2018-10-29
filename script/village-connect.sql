b2637f567b34d5cefc7bdcbf80e613efc551ebf5
Kumar, Ranjeet <ranjeet.kumar@rakuten.com>
2018-10-30 00:13:18 +0530

    updated sql script

--- script/village-connect.sql
+++ script/village-connect.sql
@@ -7,3 +7,40 @@ CREATE TABLE `villagae-connect`.`user` (
   `createdDate` TIMESTAMP NOT NULL DEFAULT now(),
   `modifyDate` TIMESTAMP NOT NULL DEFAULT now(),
   PRIMARY KEY (`id`));
+
+ALTER TABLE `village-connect`.`user` 
+ADD COLUMN `village` VARCHAR(45) NOT NULL AFTER `modifyDate`,
+ADD COLUMN `district` VARCHAR(45) NOT NULL AFTER `village`;
+
+ALTER TABLE `village-connect`.`user` 
+ADD COLUMN `pincode` INT NOT NULL AFTER `district`;
+
+-- student
+
+CREATE TABLE `village-connect`.`student` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `class` VARCHAR(45) NOT NULL,
+  `gender` VARCHAR(10) NOT NULL,
+  `college` VARCHAR(45) NOT NULL,
+  `collegeAddress` VARCHAR(45) NOT NULL,
+  `economyType` VARCHAR(45) NOT NULL,
+  `annualIncome` VARCHAR(45) NOT NULL,
+  `address` VARCHAR(100) NOT NULL,
+  `fatherName` VARCHAR(45) NOT NULL,
+  `motherName` VARCHAR(45) NOT NULL,
+  `parentEducation` VARCHAR(45) NOT NULL COMMENT 'Highest income of parent',
+  `status` INT UNSIGNED NOT NULL DEFAULT 1,
+  `createdDate` TIMESTAMP NOT NULL DEFAULT now(),
+  `modifyDate` TIMESTAMP NOT NULL DEFAULT now(),
+  PRIMARY KEY (`id`));
+
+
+ALTER TABLE `village-connect`.`student` 
+ADD INDEX `user-user_id_idx` (`user_id` ASC);
+ALTER TABLE `village-connect`.`student` 
+ADD CONSTRAINT `user-user_id`
+  FOREIGN KEY (`user_id`)
+  REFERENCES `village-connect`.`user` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
