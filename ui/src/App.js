a20ed56dead16f5dd12da0deb04650b38f7c526d
Shefali Lachhar <shefali.lachhar@gmail.com>
2018-11-02 09:26:08 +0530

    village connect react ui

---
+++ ui/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Login from "./components/Login/login";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <Login />;
+  }
+}
+
+export default App;
